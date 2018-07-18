4c2d8bce8c2250b016e116f53beaa61be2642f1a
Ryan Blecher <notryanb@gmail.com>
2018-07-18 13:08:48 -0400

    Add tests and description

---
+++ ScorpionExercise/ScorpionExercise.Tests/ExtensionMethodsTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ScorpionExercise.Tests
+{
+    public class MainTests
+    {
+        [TestFixture]
+        public class GroupingTests
+        {
+            [Test]
+            public void ChunksAreCorrectSize16()
+            {
+                var result = "1111222233334444".Chunk(16);
+                var expected = new List<string>() { "1111222233334444" };
+                Assert.That(result, Is.EquivalentTo(expected));
+            }
+
+            [Test]
+            public void ChunksAreCorrectSize2()
+            {
+                var result = "1111222233334444".Chunk(2);
+                var expected = new List<string>() { "11", "11", "22", "22", "33", "33", "44", "44" };
+                Assert.That(result, Is.EquivalentTo(expected));
+            }
+
+            [Test]
+            public void ChunksAreCorrectSize4()
+            {
+                var result = "1111222233334444".Chunk(4);
+                var expected = new List<string>() { "1111", "2222", "3333", "4444" };
+                Assert.That(result, Is.EquivalentTo(expected));
+            }
+
+            [Test]
+            public void ChunksAreCorrectSize5()
+            {
+                var result = "1111222233334444".Chunk(4);
+                var expected = new List<string>() { "11112", "22233", "33444", "4" };
+                Assert.That(result, Is.EquivalentTo(expected));
+            }
+
+            [Test]
+            public void ChunksAreCorrectSize100()
+            {
+                var result = "1111222233334444".Chunk(100);
+                var expected = new List<string>() { "1111222233334444" };
+                Assert.That(result, Is.EquivalentTo(expected));
+            }
+
+            [Test]
+            public void ZeroSizedString()
+            {
+                var result = "".Chunk(10);
+                var expected = new List<string>();
+                Assert.That(result, Is.EquivalentTo(expected));
+            }
+
+            [Test]
+            public void CatchesEdgeCaseofEmptyGrouping()
+            {
+                Assert.Throws<InvalidGroupingSizeException>(() => "1111222233334444".Chunk(0));
+            }
+        }
+    }
+}
--- ScorpionExercise/ScorpionExercise/Program.cs
+++ ScorpionExercise/ScorpionExercise/Program.cs
@@ -11,5 +11,26 @@ namespace ScorpionExercise
         static void Main(string[] args)
         {
         }
+
+    }
+
+    /// <summary>
+    /// Task:
+    ///     Complete the body of this method so it passes all NUnit tests.
+    ///     
+    /// Objective:
+    ///     Take a string and a grouping size then split the string into substrings of that group size.
+    ///     
+    ///     ex. "This is a string".Chunk(4) => List<string>() { "This", " is ", "a st", "ring" }
+    ///     
+    /// Help:
+    ///     To run tests, use `Ctrl+R, A` or use Test > Run > All Tests
+    /// </summary>
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<string> Chunk(this string sourceString, int groupSize)
+        {
+        
+        }
     }
 }
